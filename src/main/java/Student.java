a8bc9b87f0c3e40cea8ab803413aeb6b56a4a8f7
raja <kunchala@yahoo.com>
2021-09-22 16:43:16 +0100

    Commit the completed Student object
    Forgot to commit after coding this.

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Student {
+public class Student {
+    private String name;
+    private char grade;
+    private int group;
+    private final String SECRET_NICK_NAME ="MySecretNickName";
+
+    public Student(String name, char grade, int group) {
+        this.name = name;
+        this.grade = grade;
+        this.group = group;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public char getGrade() {
+        return grade;
+    }
+
+    public int getGroup() {
+        return group;
+    }
+    public void upgrade(char grade){
+        this.grade = grade;
+    }
+    public void downgrade(char grade){
+        this.grade = grade;
+    }
+
 }
